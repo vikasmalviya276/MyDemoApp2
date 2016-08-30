2c9ffaa685d4af9b0a61d7c4451cc33a0cedae98
vikasmalviya276 <vikas.malviya276@gmail.com>
2016-08-31 01:13:10 +0530

    just text

--- app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
+++ app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //set layout
+        //set layout here
         setContentView(R.layout.activity_main);
     }
 }
