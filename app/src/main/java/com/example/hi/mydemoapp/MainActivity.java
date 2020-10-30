a2756049cef58450e206cb92725182cbac2e0d1a
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-10-30 22:57:26 +0530

    @authr \vikas malviya

--- app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
+++ app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.hi.mydemoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //set layout here
         setContentView(R.layout.activity_main);
-/*vikas*/
+        /*vikas*/
+        showAdarsh();
+    }
+
+    private void showAdarsh() {
     }
 }
