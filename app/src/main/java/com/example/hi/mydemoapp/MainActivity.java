e42c7d26a179dc0d2dcb48090344e6584649b3b7
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-11-21 16:40:06 +0530

    Revert "Revert "@author vikas malviya""
    This reverts commit a92d764

--- app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
+++ app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
@@ -9,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*rachit*/
-        /*sharma*/
-        /*Mobile Number 8878634517*/
     }
 }
