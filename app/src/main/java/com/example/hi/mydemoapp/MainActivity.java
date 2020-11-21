59192141494011aa1ec030748093621e5baa2ac2
vikasmalviya276 <vikas.malviya276@gmail.com>
2020-11-21 17:19:22 +0530

    Merge branch 'new_feature_rachit' of https://github.com/vikasmalviya276/MyDemoApp2 into new_feature_vikas
    # Conflicts:
    #	app/src/main/java/com/example/hi/mydemoapp/MainActivity.java

--- app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
+++ app/src/main/java/com/example/hi/mydemoapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*vikas*/
-        /*malviya*/
+        /*vikas malviya*/
+        /*mobile number 8878634517*/
     }
 }
